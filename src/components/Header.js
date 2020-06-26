b7970a8736075e79afe4a30abfe0356532fc3e6b
YOUR USERNAMErobpwatkins <robpwatkins@gmail.com>
2020-06-26 17:28:57 -0500

    gonna try this one with css modules

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const HeaderDiv = styled.div`
-  height: 50px;
-  width: 100%;
-  color: white;
-  background: #87dfd6;
-`;
-
 const Header = () => {
   return (
-    <HeaderDiv className="header">Sober Keeper</HeaderDiv>
+    <div className="header">Sober Keeper</div>
   )
 }
 
