b93e8bbac88a2532cceadfa54a0bdb7fc09f29b0
YOUR USERNAMErobpwatkins <robpwatkins@gmail.com>
2020-06-27 18:12:21 -0500

    annnd were routing

--- src/Router.js
+++ src/Router.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Switch, Route } from 'react-router';
 import Landing from './components/Landing';
+import Home from './components/Home';
 
 const Router = () => {
   return (
     <Switch>
       <Route exact path="/" component={Landing} />
+      <Route  path="/home" component={Home} />
     </Switch>
   )
 }
