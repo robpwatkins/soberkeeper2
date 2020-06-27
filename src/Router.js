34130f945f6810b9c22c0934cfb7ca325180dbf9
YOUR USERNAMErobpwatkins <robpwatkins@gmail.com>
2020-06-27 18:02:09 -0500

    added switch

--- src/Router.js
+++ src/Router.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import { Switch, Route } from 'react-router';
+import Home from './components/Home';
 
 const Router = () => {
-
+  return (
+    <Switch>
+      <Route exact path="/" component={Home} />
+    </Switch>
+  )
 }
 
 export default Router;
