76f01bee8d8ff7fd293a5f86392e9bfb241975fa
YOUR USERNAMErobpwatkins <robpwatkins@gmail.com>
2020-06-29 18:48:18 -0500

    booooom. server running on same port as app by using a proxy

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 4000;
+
+app.use('/ping', (request, response) => {
+  return response.send('pong');
+})
 
 app.listen(port, () => {
   console.log('Im listening!');
