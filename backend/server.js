f7417c551127b1baaa8a4f7a6dee9a81ad3bf70d
YOUR USERNAMErobpwatkins <robpwatkins@gmail.com>
2020-06-29 19:14:53 -0500

    servin

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,24 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 5000;
 
-app.use('/ping', (request, response) => {
-  return response.send('pong');
+app.use(express.static(path.join(__dirname, 'soberkeeper/build')));
+
+app.get('/ping', (req, res) => {
+  res.send('pong')
 })
 
-app.listen(port, () => {
-  console.log('Im listening!');
-})
+app.get('/api/getList', (req, res) => {
+  const list = ['item1', 'item2', 'item3'];
+  res.json(list);
+  console.log('Sent that shit.');
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/soberkeeper/build/index.html'));
+});
+
+app.listen(port);
+
+console.log(`I'm listening! ${port}`);
