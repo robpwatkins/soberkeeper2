4b2dd97dcd9fe2f4f62d9e710bff98736ad16a82
YOUR USERNAMErobpwatkins <robpwatkins@gmail.com>
2020-06-29 19:39:34 -0500

    switching to a test repo

--- backend/server.js
+++ backend/server.js
@@ -5,19 +5,14 @@ const port = process.env.PORT || 5000;
 
 app.use(express.static(path.join(__dirname, 'soberkeeper/build')));
 
-app.get('/ping', (req, res) => {
-  res.send('pong')
-})
-
 app.get('/api/getList', (req, res) => {
-  const list = ['item1', 'item2', 'item3'];
+  let list = ['item1', 'item2', 'item3'];
   res.json(list);
-  console.log('Sent that shit.');
 });
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/soberkeeper/build/index.html'));
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname+'/soberkeeper/build/index.html'));
+// });
 
 app.listen(port);
 
